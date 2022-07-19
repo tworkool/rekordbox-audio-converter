13e8ea21ce018ae4e13b547209b9abbfe8463039
Oliver Tworkowski <olitworkowski@gmail.com>
2022-07-19 18:05:51 +0200

    improve converter settings and naming for converted files

--- src/converter.py
+++ src/converter.py
@@ -5,18 +5,26 @@ from pathlib import Path
 from pydub import AudioSegment
 from pydub.utils import mediainfo
 
-current_path = Path(__file__).parent.resolve()
-
 
 class AudioConverter:
     def __init__(self) -> None:
-        self.file_filter = 'flac|m4a'
-        self.converted_files_dir_name = 'converted'
+        self.settings = {
+            "file_filter": 'flac|m4a',
+            "export_format": 'wav',
+            "converted_files_dir_name": 'converted',
+            "remove_converted_files": False,
+            "mirror_file_structure": True,
+            "file_name_modifications": {
+                "title_separator": '-',
+                "custom_regex_replacement": None,
+                "recreate_file_name_from_metadata": True
+            }
+        }
 
     def generate_file_tree(self, path, mirror_file_structure=True):
         root_path_parent = str(Path(path).parent.resolve())
         root_target_folder_name = path[len(root_path_parent)+1:]
-        converted_files_dir_abs = f"{root_path_parent}/{root_target_folder_name}_{self.converted_files_dir_name}-{int(time())}"
+        converted_files_dir_abs = f"{root_path_parent}/{root_target_folder_name}_{self.settings['converted_files_dir_name']}-{int(time())}"
         Path(converted_files_dir_abs).mkdir(parents=True, exist_ok=True)
 
         for root, subdirs, files in walk(path):
@@ -37,55 +45,31 @@ class AudioConverter:
 
     def is_file_type_correct(self, file) -> bool:
         file_str = str(file)
-        regex = re.findall(f'.*\.({self.file_filter})', file_str)
+        regex = re.findall(f'.*\.({self.settings["file_filter"]})', file_str)
         return (len(regex) == 1) and ('\n' not in file_str)
 
-    def convert_file_to_wav(self, file_path: Path, target_path, remove_after=False):
+    def convert_file_to_wav(self, file_path: Path, target_path):
         flac_tmp_audio_data = AudioSegment.from_file(
             file_path, file_path.suffix[1:])
-        new_file_name = file_path.name.replace(file_path.suffix, '')
+        
+        new_file_name = ""
+        old_file_info = mediainfo(str(file_path)).get('TAG', {})
+        file_name_modifications = self.settings['file_name_modifications']
 
-        complete_file_name = f"{target_path}\\{new_file_name}.wav"
+        if file_name_modifications['recreate_file_name_from_metadata'] and (old_file_info['artist'] and old_file_info['title']):
+            new_file_name = f"{old_file_info['artist']} {file_name_modifications['title_separator']} {old_file_info['title']}"
+        else:
+            new_file_name = file_path.name.replace(file_path.suffix, '').strip()
+
+        print(f"\t\t-> {new_file_name}")
+
+        complete_file_name = f"{target_path}\\{new_file_name}.{self.settings['export_format']}"
         flac_tmp_audio_data.export(
             complete_file_name,
-            format="wav",
-            tags=mediainfo(f"{file_path}\\{file_path.name}").get('TAG', {})
+            format=self.settings['export_format'],
+            tags=old_file_info
         )
 
-        if remove_after:
+        if self.settings['remove_converted_files']:
             # remove original file
             file_path.unlink()
-
-
-""" def main():
-    print(current_path)
-    index = 0
-    for path in Path('D:\\Audio\\backup\\Contents').rglob('*.flac'):
-        index += 1
-        file_path = f"{path.parent.resolve()}\\"
-        print(index, path.name)
-        flac_tmp_audio_data = AudioSegment.from_file(path, path.suffix[1:])
-
-        new_file_name = path.name.replace(path.suffix, '')
-        new_file_name_list = new_file_name.split('-')
-        # remove leading song number
-        if (len(new_file_name_list) > 0 and new_file_name_list[0].strip().isnumeric()):
-            tmp_fl_name = ""
-            for i in range(1, len(new_file_name_list)):
-                tmp_fl_name += new_file_name_list[i]
-                tmp_fl_name += ' - '
-                tmp_fl_name.strip()
-            new_file_name = tmp_fl_name
-
-        # export wav file
-        flac_tmp_audio_data.export(
-            f"{file_path}{new_file_name}.wav",
-            format="wav",
-            tags=mediainfo(f"{file_path}\\{path.name}").get('TAG', {})
-        )
-        # remove flac file
-        path.unlink()
-
-
-if __name__ == '__main__':
-    main() """
