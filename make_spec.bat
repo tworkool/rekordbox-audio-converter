d68dd31518df23d319b8405b0eae9cfd24d3ec40
Oliver Tworkowski <olitworkowski@gmail.com>
2023-04-09 18:20:13 +0200

    update make_spec batch file to add custom python statements automatically

--- make_spec.bat
+++ make_spec.bat
@@ -1,10 +1,13 @@
 @echo off
-echo generating executable file...
+echo generating executable spec file...
 
-pyi-makespec --onefile --windowed --name AudioToWavConverter src/main.py
+rem Name of to be generated spec file
+set SPEC_FILE_NAME=AudioToWavConverterrr
 
 rem Remove '--windowed' if you would like to see console outputs
+pyi-makespec --onefile --windowed --name %SPEC_FILE_NAME% src/main.py
 
-rem IMPORTANT: Add this to end of generated spec file to copy the config file automatically
-rem import shutil
-rem shutil.copyfile('src/config.ini', '{0}/config-sample.ini'.format(DISTPATH))
+rem Add Python statement to copy config.ini to build folder
+ECHO.>>"%SPEC_FILE_NAME%.spec"
+ECHO import shutil>>"%SPEC_FILE_NAME%.spec"
+ECHO shutil.copyfile('src/config.ini', '{0}/config-sample.ini'.format(DISTPATH))>>"%SPEC_FILE_NAME%.spec"
